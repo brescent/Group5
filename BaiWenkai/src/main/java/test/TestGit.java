85151c0a7a13ad04a28d390636e6b7b643b93282
ratelMan <1445401032@qq.com>
2020-03-06 17:24:20 +0800

    上传测试

--- BaiWenkai/src/main/java/test/TestGit.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class TestGit {
-
-    public static void main(String[] args){
-        System.out.println("111");
-    }
-}
--- WuHaiyuan/src/main/java/com/project/Test20200306/SubmitTest.java
+++
@@ -1,7 +0,0 @@
-package com.project.Test20200306;
-
-public class SubmitTest {
-    public static void main(String[] args) {
-        System.out.println("测试GitHub提交");
-    }
-}
