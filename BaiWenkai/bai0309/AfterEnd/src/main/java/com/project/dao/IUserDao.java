e15a4f24b5bb941aa98b1c6daedf3393d2bcbe4a
Brescent <826271694@qq.com>
2020-03-09 20:30:55 +0800

    白文楷 3-9 前后端分离1+2

---
+++ BaiWenkai/bai0309/AfterEnd/src/main/java/com/project/dao/IUserDao.java
@@ -0,0 +1,16 @@
+package com.project.dao;
+
+import com.project.entity.UserEntity;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+
+public interface IUserDao {
+
+    @Select("select * from t_user where pk_id = #{id}")
+    @ResultMap("userRM")
+    UserEntity findUser(int id);
+
+    @Insert("insert into t_user(f_name,f_pwd,f_age)values(#{name},#{pwd},#{age})")
+    void addUser(UserEntity user);
+}
---
+++ BaiWenkai/bai0309/FrontEnd/src/test/java/test/TestUser.java
@@ -0,0 +1,15 @@
+package test;
+
+import com.project.entity.UserEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class TestUser {
+
+}
