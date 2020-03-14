28bd3d7a3c89aff75b32547a1ec7864f79e8674c
Brescent <826271694@qq.com>
2020-03-14 15:45:37 +0800

    白文楷 3-14 Hibernate 简单CRUD

---
+++ BaiWenkai/bai0314/hibernateExercise/src/main/java/com/project/hibernate/entity/UserEntity.java
@@ -0,0 +1,60 @@
+package com.project.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "sys_user")
+@Entity
+public class UserEntity {
+    @Id
+    private int userId;
+
+    private String userName;
+
+    private String password;
+
+    private int age;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(int userId, String userName, String password, int age) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.age = age;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
